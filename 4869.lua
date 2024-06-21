f667e421984cba02f52299a66a8fa85b581d0502
KurumiHub <kurumiyosuki@gmail.com>
2024-06-21 09:56:47 +0700

    Update 4869.lua

--- 4869.lua
+++ 4869.lua
@@ -1,6 +1,5 @@
 local gamelist = {
-        [17545130379] = "https://raw.githubusercontent.com/KurumiHub/KurumiAll/main/RoyalSeas.lua",
-        [2961111240] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/MonsterGhoul.lua",
+        [2961111240] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/MONSTERGHOUL.lua"
     }
     local checkgame = gamelist[game.gameId]
 
